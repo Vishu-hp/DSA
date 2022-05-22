99feada2bcad6bdcf57ac32ccce2ad776ae3e913
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-05-23 00:56:12 +0530

    Create transformToLeftClonedTree.cpp

---
+++ Binary Tree/transformToLeftClonedTree.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left = nullptr;
+    Node *right = nullptr;
+    Node(int data,Node *left,Node *right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+  int idx = 0;
+Node *constructTree(vector<int> &arr)
+{
+
+    if (idx == arr.size() || arr[idx] == -1)
+    {
+        idx++;
+        return nullptr;
+    }
+    Node *node = new Node(arr[idx++],nullptr,nullptr);
+    node->left = constructTree(arr);
+    node->right = constructTree(arr);
+    return node;
+}
+
+void display(Node *node) {
+    if (node == nullptr) {
+      return;
+    }
+
+    string str = "";
+    str += node->left == nullptr ? "." : to_string(node->left->data) + "";
+    str += " <- " + to_string(node->data) + " -> ";
+    str += node->right == nullptr ? "." : to_string(node->right->data) + "";
+    cout<<str<<endl;
+
+    display(node->left);
+    display(node->right);
+}
+
+
+  Node *createLeftCloneTree(Node *node){
+    // write your code here
+    if(node==NULL){
+        return NULL;
+    }
+    Node* ans=node;
+    Node* leftAns = createLeftCloneTree(node->left);
+    Node* rightAns = createLeftCloneTree(node->right);
+
+    ans->right = rightAns;
+    Node* clone = new Node(node->data,NULL,NULL);
+    ans->left=clone;
+    ans->left->left = leftAns;
+    return ans;
+  }
+
+  
+  int main()
+    {
+        vector<int> arr;
+        int n;
+        cin>>n;
+        
+        for(int i = 0; i<n; i++){
+            string inp;
+            cin>>inp;
+            if(inp != "n"){
+                arr.push_back(stoi(inp));
+            }
+            else{
+                arr.push_back(-1);
+            }
+        }
+        
+    
+        Node *root = constructTree(arr);
+        root = createLeftCloneTree(root);
+        display(root);
+    }
