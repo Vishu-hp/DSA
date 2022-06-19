2426b1fc9c6202227726cfef90aad61ea32951fe
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-06-19 23:33:11 +0530

    Create longestCommonSubstring.cpp

---
+++ Dynamic Programming Level2/longestCommonSubstring.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+
+    vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1));
+    int ans=0;
+    for(int i=0;i<s1.length()+1;i++){
+        for(int j=0;j<s2.length()+1;j++){
+            if(i==0 || j==0){
+                dp[i][j]=0;
+            }
+            else if(s1[i-1]==s2[j-1]){
+                dp[i][j] = dp[i-1][j-1]+1;
+            }
+            else{
+                dp[i][j]=0;
+            }
+
+            if(ans<dp[i][j]){
+                ans=dp[i][j];
+            }
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
