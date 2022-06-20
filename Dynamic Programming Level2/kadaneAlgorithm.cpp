17921450d32aa294e42a42c38e8c5184cd9039e8
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-06-20 16:34:59 +0530

    Create kadaneAlgorithm.cpp

---
+++ Dynamic Programming Level2/kadaneAlgorithm.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int currSum=arr[0];
+    int bestSum=arr[0];
+    for(int i=1;i<n;i++){
+        if(currSum<0){
+            currSum = arr[i];
+        }
+        else{
+            currSum += arr[i];
+        }
+
+        if(currSum>bestSum){
+            bestSum=currSum;
+        }
+    }
+
+    cout<<bestSum<<endl;
+    return 0;
+}
